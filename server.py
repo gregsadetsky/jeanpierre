86cead8fcde4c40e4dabf10e6e8756ed4d896af7
gregsadetsky <lepetitg@gmail.com>
2024-06-12 20:28:21 -0400

    .

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
@@ -9,6 +9,11 @@ app = Flask(__name__)
 def hello_world():
     print("new web request")
     return f"hello from disco!!! the datetime is {datetime.now()}"
+
+
+@app.route("/img")
+def img():
+    return send_file("/images/image.jpg", mimetype="image/jpeg")
 
 
 if __name__ == "__main__":
