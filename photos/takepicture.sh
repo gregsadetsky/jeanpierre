a4dd174796efb720f14abbdf4233aef246e47986
gregsadetsky <lepetitg@gmail.com>
2024-06-13 22:55:01 -0400

    .

---
+++ photos/takepicture.sh
@@ -0,0 +1,2 @@
+streamer -f jpeg -o /var/lib/docker/volumes/disco-project-dee9b806c3ce4892b5c22b70b0c1034a-images/_data/image.jpeg
+mv /var/lib/docker/volumes/disco-project-dee9b806c3ce4892b5c22b70b0c1034a-images/_data/image.jpeg /var/lib/docker/volumes/disco-project-dee9b806c3ce4892b5c22b70b0c1034a-images/_data/$(uuidgen).jpg
